586be04a9873db42292d49d2e58b1299346351b3
Abder011 <Abderrahmanesehbi2@gmail.com>
2025-09-23 11:51:20 +0100

    first art

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Teste from './Teste';
+
+function App() {
+  return (
+    <>
+      <Teste/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Teste.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Teste() {
+  return (
+    <div className='bg-green-400'>Teste</div>
+  )
+}
